be2054724b92dd09b234be8ac80bae4ecde6e684
Alicia Ramirez <aramirez@rrc.ca>
2019-09-12 14:18:27 -0500

    Added a loop to index, and single.php

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 
 </head>
 
-<body>
+<body <?php body_class(); ?>>
 
     <h1>
         <?php bloginfo('name'); ?>
--- single.php
+++ single.php
@@ -1,26 +1,25 @@
 <?php get_header(); ?>
-<h1>index.php</h1>
+<h1>single.php</h1>
 <?php if (have_posts()) :
     while (have_posts()) :
         the_post(); ?>
         <article <?php post_class(); ?>>
             <h2>
-                <a href="<?php the_permalink() ?>">
-                    <?php the_title(); ?>
-                </a>
+                <?php the_title(); ?>
             </h2>
             <p>by <?php the_author(); ?></p>
             <p><?php the_time('F j, Y, g:i a'); ?></p>
 
-            <?php //the_content("more..."); 
-                    ?>
-            <?php the_excerpt(); ?>
+            <?php the_content(); ?>
+
+            <p>Categories: <?php the_category(", "); ?></p>
+            <p><?php the_tags(); ?></p>
 
         </article>
 <?php endwhile;
 endif; ?>
 
-<?php the_posts_navigation(); ?>
+<?php the_post_navigation(); ?>
 
 <?php get_sidebar(); ?>
 
